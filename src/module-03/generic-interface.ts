aa406ea6075686d57e8a37fd14bf106b1d620fa4
Rubayet-billah <rubayetbillah909@gmail.com>
2023-05-03 20:01:55 +0600

    crush interface

--- src/module-03/generic-interface.ts
+++ src/module-03/generic-interface.ts
@@ -1,1 +1,15 @@
 // Generic Interface
+interface CrushInterface<T> {
+  name: string;
+  husband: T;
+}
+
+const crush1: CrushInterface<boolean> = {
+  name: "Clara Ema",
+  husband: true,
+};
+
+const crush2: CrushInterface<string> = {
+  name: "Clara Ema",
+  husband: "Rubayet",
+};
