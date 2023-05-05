f0438c1e87c41f13906f95baa6c47e9326dec97a
Rubayet-billah <rubayetbillah909@gmail.com>
2023-05-05 14:05:25 +0600

    deposit

--- src/module-04/getter-setter.ts
+++ src/module-04/getter-setter.ts
@@ -1,28 +1,30 @@
-class BankAccount {
-  id: number;
-  name: string;
-  private _balance: number; // access modifier
+// class BankAccount {
+//   id: number;
+//   name: string;
+//   private _balance: number; // access modifier
 
-  constructor(id: number, name: string, balance: number) {
-    this.id = id;
-    this.name = name;
-    this._balance = balance;
-  }
+//   constructor(id: number, name: string, balance: number) {
+//     this.id = id;
+//     this.name = name;
+//     this._balance = balance;
+//   }
 
-  // getter for access private property
-  get balance(): number {
-    return this._balance;
-  }
+//   // getter for access private property
+//   get balance(): number {
+//     return this._balance;
+//   }
 
-  getBalance() {}
-  // addBalance(amount: number) {
-  //   this._balance = this._balance + amount;
-  // }
+//   getBalance() {}
+//   // addBalance(amount: number) {
+//   //   this._balance = this._balance + amount;
+//   // }
 
-  // setter
-  set deposit(amount: number) {
-    this._balance = this._balance + amount;
-  }
-}
+//   // setter
+//   set deposit(amount: number) {
+//     this._balance = this._balance + amount;
+//   }
+// }
 
-const myAccount = new BankAccount(444, "Rubayet", 20);
+// const myAccount = new BankAccount(444, "Rubayet", 20);
+
+// myAccount.deposit = 30;
