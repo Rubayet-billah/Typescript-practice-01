4a240a6bb26095927bc8617ebf802dac01a9f0c3
Rubayet-billah <rubayetbillah909@gmail.com>
2023-05-05 13:52:46 +0600

    access modifier

---
+++ src/module-04/access-modifires.ts
@@ -0,0 +1,13 @@
+class BankAccount {
+  id: number;
+  name: string;
+  private _balance: number; // access modifier
+
+  constructor(id: number, name: string, balance: number) {
+    this.id = id;
+    this.name = name;
+    this._balance = balance;
+  }
+}
+
+const myAccount = new BankAccount(444, "Rubayet", 20);
