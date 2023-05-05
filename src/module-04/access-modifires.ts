1e205232d4f21e3bb8460207fec38933b222cbe0
Rubayet-billah <rubayetbillah909@gmail.com>
2023-05-05 14:03:56 +0600

    getter setter

--- src/module-04/access-modifires.ts
+++ src/module-04/access-modifires.ts
@@ -1,13 +1,14 @@
-class BankAccount {
-  id: number;
-  name: string;
-  private _balance: number; // access modifier
+// class BankAccount {
+//   id: number;
+//   name: string;
+//   private _balance: number; // access modifier
+//   protected _balance: number; // access modifier
 
-  constructor(id: number, name: string, balance: number) {
-    this.id = id;
-    this.name = name;
-    this._balance = balance;
-  }
-}
+//   constructor(id: number, name: string, balance: number) {
+//     this.id = id;
+//     this.name = name;
+//     this._balance = balance;
+//   }
+// }
 
-const myAccount = new BankAccount(444, "Rubayet", 20);
+// const myAccount = new BankAccount(444, "Rubayet", 20);
---
+++ src/module-04/getter-setter.ts
@@ -0,0 +1,28 @@
+class BankAccount {
+  id: number;
+  name: string;
+  private _balance: number; // access modifier
+
+  constructor(id: number, name: string, balance: number) {
+    this.id = id;
+    this.name = name;
+    this._balance = balance;
+  }
+
+  // getter for access private property
+  get balance(): number {
+    return this._balance;
+  }
+
+  getBalance() {}
+  // addBalance(amount: number) {
+  //   this._balance = this._balance + amount;
+  // }
+
+  // setter
+  set deposit(amount: number) {
+    this._balance = this._balance + amount;
+  }
+}
+
+const myAccount = new BankAccount(444, "Rubayet", 20);
