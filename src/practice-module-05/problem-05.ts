e866066332f5233e10f201437e0ecbe54e3c0f98
Rubayet-billah <rubayetbillah909@gmail.com>
2023-05-06 21:46:21 +0600

    pr 6

---
+++ src/practice-module-05/problem-05.ts
@@ -0,0 +1,15 @@
+/**
+ * Suppose you have an array of numbers in TypeScript, and you want to find the sum of all the even numbers in the array. How would you approach this problem and write code to solve it?
+ */
+
+// const sumOfEven = (arr: number[]): number => {
+//   return arr.reduce((previousValue, currentElement) => {
+//     if (currentElement % 2 !== 0) {
+//       return previousValue + currentElement;
+//     } else {
+//       return previousValue;
+//     }
+//   }, 0);
+// };
+
+// console.log(sumOfEven([2, 4, 5, 1, 6, 9]));
---
+++ src/practice-module-05/problem-06.ts
@@ -0,0 +1,3 @@
+/*
+Create an interface called Person that includes properties for name (string), age (number), and email (string). Then create an array of Person objects and write a function that takes the array and a string email as parameters, and returns the Person object that matches the email or null if no match is found.
+*/
