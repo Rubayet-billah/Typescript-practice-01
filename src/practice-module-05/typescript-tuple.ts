719d9beefc533825df011b493b0a442cb0b64e0a
Rubayet-billah <rubayetbillah909@gmail.com>
2023-05-06 14:13:05 +0600

    problem2 solved

--- src/practice-module-05/typescript-tuple.ts
+++ src/practice-module-05/typescript-tuple.ts
@@ -1,8 +1,11 @@
-const userInfo: [number, string, string, boolean, undefined, string] = [
-  101,
-  "Ema",
-  "John",
-  true,
-  ,
-  "2023",
-];
+// Convert the following JavaScript array into a TypeScript tuple
+//     const userInfo = [101, "Ema", "John", true,  , "2023"];
+
+// const userInfo: [number, string, string, boolean, undefined, string] = [
+//   101,
+//   "Ema",
+//   "John",
+//   true,
+//   ,
+//   "2023",
+// ];
